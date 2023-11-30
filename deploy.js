1ae57cf52c11a135bcbd36b053ad7afc47f4730a
Zach Truong <zacharytruong@gmail.com>
2023-12-01 01:38:52 +0700

    deploy script

--- deploy.js
+++ deploy.js
@@ -1,5 +1,26 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+  'mnemonic', // replace with your own mnemonic
+  'https://sepolia.infura.io/v3/apiKey' // replace with your own infura api key
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  accounts = await web3.eth.getAccounts();
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
